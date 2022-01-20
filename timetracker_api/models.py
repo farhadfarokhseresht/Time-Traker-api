ad61065efa3e0433d3104201b119a18f42176615
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-20 21:28:31 +0330

    add models to app

--- timetracker_api/models.py
+++ timetracker_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class TimeTracker(models.Model):
+    description = models.CharField(max_length=300)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    tags = models.CharField(max_length=300)
+    billable = models.BooleanField(default=False)
+    start_at = models.DateField()
+    end_at = models.DateField()
