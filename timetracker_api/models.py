f7a106ec952c88ed617dd3c55ffa79314e22153d
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-20 21:34:21 +0330

    modified models.py (add Tags class)

--- timetracker_api/models.py
+++ timetracker_api/models.py
@@ -7,10 +7,14 @@ class Project(models.Model):
     name = models.CharField(max_length=150)
 
 
+class Tags(models.Model):
+    name = models.CharField(max_length=150)
+
+
 class TimeTracker(models.Model):
     description = models.CharField(max_length=300)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    tags = models.CharField(max_length=300)
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
     billable = models.BooleanField(default=False)
     start_at = models.DateField()
     end_at = models.DateField()
