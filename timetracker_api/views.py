929b0ee7080037d0b992b28f31f6f10c14f95fc0
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-21 19:38:07 +0330

    define start_time_tracker fun.. to start time tracking and creat instance of TimeTracker class in models.py

--- timetracker_api/views.py
+++ timetracker_api/views.py
@@ -4,3 +4,20 @@ from timetracker_api.models import *
 import datetime
 
 
+@api_view(['POST', 'GET'])
+def start_time_tracker(request):
+    start_at = datetime.datetime.now()
+    TimeTracker_instance = TimeTracker(start_at=start_at)
+    # if request.method == 'POST':
+    if 'description' in request.data:
+        TimeTracker_instance.description = request.data['description']
+    if 'project' in request.data:
+        TimeTracker_instance.project = request.data['project']
+    if 'tags' in request.data:
+        TimeTracker_instance.tags = request.data['tags']
+    if 'billable' in request.data:
+        TimeTracker_instance.billable = request.data['billable']
+
+    print(TimeTracker_instance.billable)
+
+    return Response({"message": start_at})
