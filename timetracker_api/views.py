85263a9e008a23ac5ffd655c81bbde54c16812c6
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-21 21:56:59 +0330

    #Add a new time entry #Stop currently running timer

--- timetracker_api/views.py
+++ timetracker_api/views.py
@@ -7,11 +7,10 @@ import datetime
 
 @api_view(['POST', 'GET', 'PATCH', 'PUT', 'DELETE'])
 def time_tracker(request):
-    
     # Add a new time entry
     if request.method == 'POST':
         start_at = datetime.datetime.now()
-        TimeTracker_instance = TimeTracker(start_at=start_at)
+        TimeTracker_instance: TimeTracker = TimeTracker(start_at=start_at)
         if 'description' in request.data:
             TimeTracker_instance.description = request.data['description']
         # if 'project' in request.data:
@@ -20,7 +19,16 @@ def time_tracker(request):
         #     TimeTracker_instance.tags = request.data['tags']
         if 'billable' in request.data:
             TimeTracker_instance.billable = request.data['billable']
-        # TimeTracker_instance.end_at = start_at
+        TimeTracker_instance.save()
+        return Response({"message": "OK0"}, status=status.HTTP_200_OK)
 
-    TimeTracker_instance.save()
-    return Response({"message": "start_time_tracker id :{}".format(TimeTracker_instance.id)}, status=status.HTTP_200_OK)
+    # Stop currently running timer
+    if request.method == 'PATCH':
+        if 'TimeTracker_instance_id' in request.data:
+            TimeTracker_instance = TimeTracker.objects.get(id=request.data['TimeTracker_instance_id'])
+            print(TimeTracker_instance.description)
+            return Response({"message": "OK1"}, status=status.HTTP_200_OK)
+        else:
+            TimeTracker_instance = TimeTracker.objects.latest('id')
+            print(TimeTracker_instance.description)
+        return Response({"message": "OK2"}, status=status.HTTP_200_OK)
