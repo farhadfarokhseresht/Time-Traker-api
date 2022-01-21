f7c71d7b27490c449b6697c3ef7fe75b63bd2374
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-21 21:26:43 +0330

    تغییر نام تابع در usrls و views

--- timetracker_api/urls.py
+++ timetracker_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from timetracker_api import views
 
 urlpatterns = [
-    path("start_time_tracker", views.start_time_tracker),
+    path("time_tracker", views.time_tracker),
 ]
