a2fb5948b8292b941fdeaca10a6f7e1bb897cbfa
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-21 20:44:55 +0330

    سطون های پروژه و تگ را به طور موقت برداشته شده عملیات میگریشن روی دیتا بیس انجام شده

--- timetracker_api/models.py
+++ timetracker_api/models.py
@@ -13,8 +13,8 @@ class Tags(models.Model):
 
 class TimeTracker(models.Model):
     description = models.CharField(max_length=300,blank=True,null=True,default=None)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE,blank=True,null=True,default=None)
-    tags = models.ForeignKey(Tags, on_delete=models.CASCADE,blank=True,null=True,default=None)
+    # project = models.ForeignKey(Project, on_delete=models.CASCADE,blank=True,null=True,default=None)
+    # tags = models.ForeignKey(Tags, on_delete=models.CASCADE,blank=True,null=True,default=None)
     billable = models.BooleanField(default=False)
     start_at = models.DateTimeField()
     end_at = models.DateTimeField(blank=True,null=True,default=None)
