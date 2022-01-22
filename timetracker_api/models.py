12406169db5bdd82458f876baa21e5b8c5bc6814
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-22 14:33:38 +0330

    modified models class تابع str را به کلاس ها جهت نمایش بهتر اضاف شده

--- timetracker_api/models.py
+++ timetracker_api/models.py
@@ -4,17 +4,28 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, blank=False, null=False)
+    description = models.CharField(max_length=300, blank=True, null=True, default=None)
+
+    def __str__(self):
+        return self.name
 
 
 class Tags(models.Model):
     name = models.CharField(max_length=150)
+    description = models.CharField(max_length=300, blank=True, null=True, default=None)
+
+    def __str__(self):
+        return self.name
 
 
 class TimeTracker(models.Model):
-    description = models.CharField(max_length=300,blank=True,null=True,default=None)
+    description = models.CharField(max_length=300, blank=True, null=True, default=None)
     # project = models.ForeignKey(Project, on_delete=models.CASCADE,blank=True,null=True,default=None)
     # tags = models.ForeignKey(Tags, on_delete=models.CASCADE,blank=True,null=True,default=None)
     billable = models.BooleanField(default=False)
     start_at = models.DateTimeField()
-    end_at = models.DateTimeField(blank=True,null=True,default=None)
+    end_at = models.DateTimeField(blank=True, null=True, default=None)
+
+    def __str__(self):
+        return self.description + " start at : " + str(self.start_at) +" end at : " + str(self.end_at)
