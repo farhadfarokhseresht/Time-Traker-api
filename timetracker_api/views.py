9c272fe27a93d0ba2bd497b8df6ae54e1076ffc5
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-22 11:42:48 +0330

    add #Delete time entry to TimeTracker_set function

--- timetracker_api/views.py
+++ timetracker_api/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 import datetime
 
 
-def TimeTracker_set(request,TimeTracker_instance) :
+def TimeTracker_set(request, TimeTracker_instance):
     if 'start_at' in request.data:
         TimeTracker_instance.start_at = request.data['start_at']  # check data entry !!!
     if 'description' in request.data:
@@ -19,6 +19,7 @@ def TimeTracker_set(request,TimeTracker_instance) :
     if 'end_at' in request.data:
         TimeTracker_instance.end_at = request.data['end_at']  # check data entry !!!
     return TimeTracker_instance
+
 
 @api_view(['POST', 'GET', 'PATCH', 'PUT', 'DELETE'])
 def time_tracker(request):
@@ -50,4 +51,13 @@ def time_tracker(request):
             TimeTracker_instance.save()
             return Response({"message": "Update time entry done successfully"}, status=status.HTTP_200_OK)
         else:
-            return Response({"message": "Currently running time entry was not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": "Currently time entry was not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    # Delete time entry
+    if request.method == 'DELETE ':
+        if 'TimeTracker_id' in request.data:
+            TimeTracker_instance = TimeTracker.objects.get(id=request.data['TimeTracker_id'])  # check data entry !!!
+            TimeTracker_instance.delete()
+            return Response({"message": "Delete time entry done successfully"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Time entry with given ID doesn't exist or doesn't belong"}, status=status.HTTP_404_NOT_FOUND)
