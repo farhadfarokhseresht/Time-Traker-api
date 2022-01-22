820e4e0ea0443ecbb4d2cd32f4635c7fd046c720
farhad farokhseresht <farhad.farokhseresht@gmail.com>
2022-01-23 00:07:31 +0330

    edite TimeTracker_serializers add meta clss define : create , update

--- timetracker_api/serializers.py
+++ timetracker_api/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
+from .models import *
+import datetime
 
 
-class TimeTracker_serializers(serializers.Serializer):
-    description = serializers.CharField()
-    billable = serializers.BooleanField(required=True)
-    start_at = serializers.DateTimeField(required=True)
-    end_at = serializers.DateTimeField(required=False)
+class TimeTracker_serializers(serializers.ModelSerializer):
+    class Meta:
+        model = TimeTracker
+        fields = '__all__'
+
+    def create(self, validated_data):
+        if validated_data['start_at'] > validated_data['end_at']:
+            raise serializers.ValidationError("The start time must be later than the end time !")
+        else:
+            obj = super().create(validated_data)
+            obj.save()
+            return obj
+
+    def update(self, instance, validated_data):
+
+        if validated_data['start_at'] > validated_data['end_at']:
+            raise serializers.ValidationError("The start time must be later than the end time !")
+        else:
+            obj = super().update(instance, validated_data)
+            obj.save()
+            return obj
